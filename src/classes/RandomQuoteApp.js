e2d776b1df3bd94b3bd75c0a4c3bae52508acb31
AndrewSide <agorban2396@gmail.com>
2025-04-16 13:38:43 +0300

    Format quote text and author in the Quote class

--- src/classes/RandomQuoteApp.js
+++ src/classes/RandomQuoteApp.js
@@ -10,9 +10,8 @@ class RandomQuoteApp {
   }
 
   displayCurrentQuote() {
-    const { text, author } = this.currentQuote;
-    this.quoteTextElement.textContent = `${text}`;
-    this.quoteAuthorElement.textContent = author;
+    this.quoteTextElement.textContent = this.currentQuote.formatText();
+    this.quoteAuthorElement.textContent = this.currentQuote.formatAuthor();
   }
 
   getRandomQuote() {
